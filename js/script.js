84b9b2c5bbc920f2bce2c1ef085a2b6a825c9cb5
boogie27 <anonyecharles@gmail.com>
2020-09-11 02:50:39 +0100

    quest

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-   
+    //=================================
+    //      SEARCH BOX OPEN
+    // ================================
+var searchBoxOpenBtn = $("#searchBoxOpenBtn");
+    $(searchBoxOpenBtn).click(function(){
+          $("#mobileSearchBar").slideToggle(150);
+    });
+
+
+
+
+
+    // end of document
 });
