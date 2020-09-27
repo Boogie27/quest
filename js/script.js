0f6487a38bb342202ff876645b426f85694db6cc
boogie27 <anonyecharles@gmail.com>
2020-09-27 02:34:00 +0100

    quest

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
 $(document).ready(function(){
-    //=================================
-    //      SEARCH BOX OPEN
-    // ================================
+
+//============================================
+//      INITIALIZATION FUNCTION
+// ===========================================
+function initalization(){
+    $(".light-skin").hide();
+    $("#leftSideBar").hide(100);
+}
+initalization()
+//==========================================
+//      SEARCH BOX OPEN AND CLOSE
+// =========================================
 var searchBoxOpenBtn = $("#searchBoxOpenBtn");
     $(searchBoxOpenBtn).click(function(){
-          $("#mobileSearchBar").slideToggle(150);
-    });
+    $("#desktopSearchBar .search-bar").slideToggle(150);
+});
 
+//============================================
+//      MOBILE SIDE NAVIGATION  OPEN
+// ===========================================
+var leftSideNavBtn = $("#leftSideNavBtn");
+    $(leftSideNavBtn).click(function(){
+    $(".light-skin").toggle();
+    $("#leftSideBar").show(150);
+});
+
+//============================================
+//      MOBILE SIDE NAVIGATION  OPEN AND CLOSE
+// ===========================================
+var leftSideNavCancleBtn = $("#leftSideNavCancleBtn");
+    $(leftSideNavCancleBtn).click(function(){
+        initalization();
+});
+
+//============================================
+//      MOBILE LIGHT SKIN CLOSE
+// ===========================================
+var lightSkin = $(".light-skin");
+    $(lightSkin).click(function(){
+        initalization();
+});
 
 
 
